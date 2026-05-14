29d09f75639d212ee6f0a27c86ad3a477327613c
zhhsh <33864086+zhhsh@users.noreply.github.com>
2026-05-14 09:21:59 +0800

    Do practices 01~05 in foundations

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,9 +8,15 @@
 */
 
 // Add your code right below, good luck!
+firstName = 'Carlos'
+lastName = 'Stevenson'
+thisYear = 1965
+birthYear = 1947
 
+fullName = `${firstName} ${lastName}`
+age = thisYear - birthYear
 
-
+greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 
 // Do not change this
